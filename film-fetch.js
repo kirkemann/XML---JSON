7decb5399af16a718ded3c12ea0b41f424f60b6f
Kirkemann <kirkemann35@gmail.com>
2019-11-05 14:12:46 +0100

    det sidste

--- film-fetch.js
+++ film-fetch.js
@@ -2,28 +2,42 @@ let pageCounter = 1;
 let filmContainer = document.getElementById("film-info");
 let btn = document.getElementById("btn");
 
-btn.addEventListener("click", function() { 
+btn.addEventListener("click", function() {
+    
+let wsurl = 'https://raw.githubusercontent.com/kirkemann/XML---JSON/master/film-' + pageCounter + '.json';
 
-    let myFilm = new XMLHttpRequest();
+fetch(wsurl, {
+    method: 'GET'
+}).then(function(data) {
+    return data.json();
+}).then(function(jsondata) {
+    renderHTML(jsondata);
+}).catch(function(error) {
+    console.log("Noget gik galt" + error);  
+})
 
-myFilm.open('GET', 'https://raw.githubusercontent.com/kirkemann/XML---JSON/master/film-' + pageCounter + '.json');
 
-myFilm.onload = function() {
-    if (myFilm.status >= 200 && myFilm.status < 400) {
-        let ourData = JSON.parse(myFilm.responseText);
-        renderHTML(ourData);
-    } else {
-        console.log("We connected to the server, but it returned an error.");
+
+//     let myFilm = new XMLHttpRequest();
+
+// myFilm.open('GET', 'https://raw.githubusercontent.com/kirkemann/XML---JSON/master/film-' + pageCounter + '.json');
+
+// myFilm.onload = function() {
+//     if (myFilm.status >= 200 && myFilm.status < 400) {
+//         let ourData = JSON.parse(myFilm.responseText);
+//         renderHTML(ourData);
+//     } else {
+//         console.log("We connected to the server, but it returned an error.");
         
-    }
+//     }
 
-};
+// };
 
-myFilm.onerror = function() {
-    console.log("Connection error");
-}
+// myFilm.onerror = function() {
+//     console.log("Connection error");
+// }
 
-myFilm.send();
+// myFilm.send();
 pageCounter++;
 if (pageCounter > 3) {
     btn.classList.add("hide");
